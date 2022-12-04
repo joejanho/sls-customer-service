4de92ad6b5bf539669c66aa89ca8977e09e88b4d
joejanho <joejanho@gmail.com>
2022-12-05 00:20:14 +0100

    Update index.ts

--- app/src/errors/index.ts
+++ app/src/errors/index.ts
@@ -1,5 +1,1 @@
-export * from './payment-invalid-error';
 export * from './validation-error';
-export * from './subscription-already-upgraded-error';
-export * from './max-number-of-playlists-error';
-export * from './max-playlist-size-error';
